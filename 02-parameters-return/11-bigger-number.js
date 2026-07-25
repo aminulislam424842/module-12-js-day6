aa592c477e61af42fbb487d5bea8adb285f8e73c
Aminul islam <aminulislam424842@gmail.com>
2026-07-25 17:34:36 +0600

    feat: create function to compare two numbers

---
+++ 02-parameters-return/11-bigger-number.js
@@ -0,0 +1,12 @@
+function biggerNumber(a, b) {
+  if (a > b) {
+    return "Big number is A";
+  }
+  else if (a < b) {
+    return "Big number is B";
+  }
+  else {
+    return "Both ar Equal";
+  }
+}
+console.log(biggerNumber(4, 4));
