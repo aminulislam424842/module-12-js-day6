bc6d1ce83b074ccf4f19d6152e6bcd37b632ee0e
Aminul islam <aminulislam424842@gmail.com>
2026-07-25 22:13:26 +0600

    feat: count elements in an array

---
+++ 03-array-functions/27-count-elements.js
@@ -0,0 +1,9 @@
+function countElements(array) {
+  let count = array.length;
+  return count;
+}
+
+let array = [45, 87, 457, 24, 78, 65, 14, 2545, 21];
+let result = countElements(array);
+
+console.log(result);
