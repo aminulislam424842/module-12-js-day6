947cfc53e7104e1ebdf5088d8e9a0bb7e7880131
Aminul islam <aminulislam424842@gmail.com>
2026-07-25 19:27:40 +0600

    feat: create array sum calculation function

---
+++ 03-array-functions/21-sum-array.js
@@ -0,0 +1,13 @@
+function sumArray (array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
+let array = [1, 2, 3];
+
+const result = sumArray(array);
+
+console.log(result);
